32e566690ca26ba1c6b228c7a6b53f8d9c426dd6
Peter Tsoi <petertsoi@gmail.com>
2013-02-18 17:56:14 -0800

    Upload files to cloud

---
+++ FS/Cloud.py
@@ -0,0 +1,36 @@
+from os import chdir, listdir, path, system
+import tarfile
+
+from farmtools.Farm.Env import *
+from farmtools.FS.Sandbox import *
+
+class Cloud:
+  __clouduser__ = "farm"
+  __dnsname__ = "cloud.cs.berkeley.edu"
+  @classmethod
+  def UploadDir(cls, outputdir):
+    mybox = Sandbox()
+    if outputdir:
+      outputdir = path.join(mybox.path, outputdir)
+      if path.exists(outputdir) and path.isdir(outputdir):
+        tarfilename = ".farmout.tgz"
+        tarfilepath = path.join(mybox.path, tarfilename)
+        tar = tarfile.open(tarfilepath, "w:gz")
+        chdir(outputdir)
+        for file in listdir(outputdir):
+          tar.add(file)
+        tar.close()
+        clouduser = cls.__clouduser__
+        dnsname = cls.__dnsname__
+        owner = mybox.env.Get("FARM_JOBOWNER")
+        jid = mybox.env.Get("FARM_JOBID")
+        tid = mybox.env.Get("FARM_TASKID")
+        remotepath = path.join("/var/spool/tractor/farm-output", owner, jid, tid)
+        system("ssh -o StrictHostKeyChecking=no %(clouduser)s@%(dnsname)s 'source cnm_settings; cnm_dfmkdir -u %(owner)s -j %(jid)s -t %(tid)s'" % locals())
+        system("scp -o StrictHostKeyChecking=no %(tarfilepath)s %(clouduser)s@%(dnsname)s:%(remotepath)s/." % locals())
+        system("ssh -o StrictHostKeyChecking=no %(clouduser)s@%(dnsname)s 'cd %(remotepath)s; tar -zxf %(tarfilename)s; rm -f %(tarfilename)s'" % locals())
+      else:
+        raise Exception("UploadDir requires a directory path")
+    else:
+      raise Exception("UploadDir requires a directory path")
+
---
+++ bin/cnm_returnoutput.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import getopt
+
+from farmtools.FS.Cloud import *
+from farmtools.FS.Sandbox import *
+
+def main(argv):
+  __doc__ = "cnm_returnoutput -u <user> -j <job id> -t <task id> -o <output dir>"
+  try:
+    opts, args = getopt.getopt(argv,"h:u:j:t:o:",["user=","jid=","tid=","output="])
+  except getopt.error, msg:
+    print __doc__
+    sys.exit(2)
+  user = None
+  jid = None
+  tid = None
+  outputdir = None
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      print __doc__
+      sys.exit(0)
+    elif opt in ("-u", "--user"):
+      user = arg
+    elif opt in ("-j", "--jid"):
+      jid = arg
+    elif opt in ("-t", "--tid"):
+      tid = arg
+    elif opt in ("-o", "--output"):
+      outputdir = arg
+
+  if user and jid and tid:
+    mybox = Sandbox(owner=user, jid=jid, tid=tid)
+  elif user and jid and not tid:
+    mybox = Sandbox(owner=user, jid=jid)
+  elif user and not jid and tid:
+    mybox = Sandbox(owner=user, tid=tid)
+  elif not user and jid and tid:
+    mybox = Sandbox(jid=jid, tid=tid)
+  elif not user and not jid and tid:
+    mybox = Sandbox(tid=tid)
+  elif not user and jid and not tid:
+    mybox = Sandbox(jid=jid)
+  elif user and not jid and not tid:
+    mybox = Sandbox(owner=user)
+  else:
+    mybox = Sandbox()
+
+  if outputdir:
+    os.chdir(mybox.path)
+    if path.exists(path.join(mybox.path, outputdir)):
+      Cloud.UploadDir(outputdir)
+    else:
+      raise Exception("Output directory does not exist")
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
